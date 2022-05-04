91393ab1e405d92101e31f8ee1b578b50f4b5bca
Kamalendu07 <98376043+Kamalendu07@users.noreply.github.com>
2022-05-04 06:40:28 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'fromtest';
-}
+  title = 'formtest';
+  
+  uname=new FormControl('');
+  updateName()
+  {
+    this.uname.setValue("Kamalendu");
+  }
+  person1:number=0;
+  person2:number=0;
+  tall=false;
+  ph1=new FormControl(0);
+  ph2=new FormControl(0);
+
+  findTaller()
+  {
+    this.person1=Number(this.ph1.value);
+    this.person2=Number(this.ph2.value);
+    if (this.person1>this.person2)
+    {
+    this.tall=true;
+    }
+    else
+    {
+    this.tall=false;
+    }
+  }
+    products=[
+      {name:"Mobile",price:30000,company:"Samsung"},
+      {name:"Music System",price:34000,company:"JBL"},
+      {name:"AC",price:38000,company:"Hitachi"}
+      
+    ];
+  }
